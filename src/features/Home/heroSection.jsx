b9a02dadcde7ca93b63f7c928e9e7f6adee58a8d
MaheshVenneti <maheshvenneti123@mail.com>
2025-06-17 06:52:06 +0530

    folder structure and nav

---
+++ src/features/Home/heroSection.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ccc",
+        height: "90vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Get Best products from our Ecommerce website</h1>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeComponent from "../features/Home";
+import Navbar from "../components/NavBar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <HomeComponent />
+    </>
+  );
+};
+
+export default Home;
